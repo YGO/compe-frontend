8d1bfe9688608dcf45d08464a443be7fffce570f
Akira Sosa <akirasosa@gmail.com>
2017-09-20 14:34:03 +0700

    design done

--- src/routes/leaderboards/components/leaderboard-index2.component.js
+++ src/routes/leaderboards/components/leaderboard-index2.component.js
@@ -7,73 +7,76 @@ const PlayerIndex = () => (
   <div className='container-fluid' style={style.container}>
 
     <section style={style.section}>
-      <div id='rankingHeader' className='row'>
-        <div className='col-2' style={[style.cell, style.headerColor]}>POS.
+      <div id='rankingHeader' className='row' style={style.headerRow}>
+        <div className='col-2' style={[style.cell, style.transparent]}>POS.
         </div>
-        <div className='col-6' style={[style.cell, style.headerColor]}>Player
+        <div className='col-6' style={[style.cell, style.transparent]}>Player
         </div>
-        <div className='col-2' style={[style.cell, style.headerColor]}>Total
+        <div className='col-2' style={[style.cell, style.transparent]}>Total
           (Gross)
         </div>
-        <div className='col-2' style={[style.cell, style.headerColor]}>THRU
+        <div className='col-2' style={[style.cellStop, style.transparent]}>THRU
         </div>
       </div>
       <div className='row' style={style.playerRow}>
-        <div className='col-12 mt-1 mb-1'>
-          <div className='row' data-toggle='collapse' data-target='.scores' style={{borderBottom: '1px solid #ccc'}}>
+        <div className='col-12 mt-1 mb-1' style={style.white}>
+          <div className='row' data-toggle='collapse' data-target='.scores'
+               style={[style.borderBottom, {cursor: 'pointer'}]}>
             <div className='col-2' style={[style.cell]}>1</div>
-            <div className='col-6' style={[style.cell]}>麻田 隆司</div>
+            <div className='col-6' style={[style.cell, style.alignLeft]}><span
+              className='text-primary'>麻田 隆司</span></div>
             <div className='col-2' style={[style.cell]}>-5</div>
-            <div className='col-2' style={[style.cell, {borderRight: '0px'}]}>F</div>
+            <div className='col-2' style={[style.cellStop]}>
+              F
+            </div>
           </div>
-          <div className='row collapse scores'>
+          <div className='row collapse scores show'>
             <div className='col-12'>
               <div className='row'>
-                <div className='col-2 align-items-center' style={[style.cell]}>
-                  1日目
+                <div className='col-2' style={[style.cell]}>1日目
                 </div>
-                <div className='col-10' style={[{backgroundColor: '#fff'}]}>
+                <div className='col-10' style={[style.white]}>
                   <div className='row'>
-                    <div className='col ml-0 mr-0 pl-0 pr-0 pt-1 pb-1'>
+                    <div className='col m-0 p-0 pt-1 pb-1'>
                       <table className='table table-bordered table-sm mb-0'
-                             style={[{backgroundColor: '#eee'}]}>
+                             style={[style.gray]}>
                         <thead>
                         <tr>
                           {Array(9).fill().map((_, idx) =>
-                            <th style={{textAlign: 'center'}}>{idx + 1}</th>
+                            <th style={[style.scoreCell]}>{idx + 1}</th>
                           )}
-                          <th style={{textAlign: 'center', width: '52px'}}>OUT
+                          <th style={[style.scoreCellTotal]}>OUT
                           </th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                           {Array(9).fill().map(() =>
-                            <td style={{textAlign: 'center'}}>+3</td>
+                            <td style={[style.scoreCell]}>+3</td>
                           )}
-                          <td style={{textAlign: 'center'}}>36</td>
+                          <td style={[style.scoreCellTotal]}>36</td>
                         </tr>
                         </tbody>
                       </table>
                     </div>
-                    <div className='col ml-0 mr-0 pl-0 pr-0 pt-1 pb-1'>
+                    <div className='col m-0 p-0 pt-1 pb-1'>
                       <table className='table table-bordered table-sm mb-0'
-                             style={[{backgroundColor: '#eee'}]}>
+                             style={[style.gray]}>
                         <thead>
                         <tr>
                           {Array(9).fill().map((_, idx) =>
-                            <th style={{textAlign: 'center'}}>{idx + 1}</th>
+                            <th style={[style.scoreCell]}>{idx + 1}</th>
                           )}
-                          <th style={{textAlign: 'center', width: '52px'}}>IN
+                          <th style={[style.scoreCellTotal]}>IN
                           </th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                           {Array(9).fill().map(() =>
-                            <td style={{textAlign: 'center'}}>+3</td>
+                            <td style={[style.scoreCell]}>+3</td>
                           )}
-                          <td style={{textAlign: 'center'}}>36</td>
+                          <td style={[style.scoreCellTotal]}>36</td>
                         </tr>
                         </tbody>
                       </table>
@@ -84,7 +87,6 @@ const PlayerIndex = () => (
             </div>
           </div>
         </div>
-
       </div>
     </section>
 
--- src/routes/leaderboards/components/leaderboard.style.js
+++ src/routes/leaderboards/components/leaderboard.style.js
@@ -1,3 +1,17 @@
+const cellBase = {
+  borderRight: '1px solid #ccc',
+  padding: '4px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+const colors = {
+  accent: '#01a5a3',
+  gray: '#eee',
+  darkGray: '#ccc',
+  white: '#fff',
+}
+
 const style = {
   container: {
     maxWidth: '960px',
@@ -5,25 +19,48 @@ const style = {
   section: {
     marginTop: '16px',
   },
-  // playerRow
-  playerRow: {
-    backgroundColor: '#eee',
-    borderLeft: '4px solid #eee',
-    borderRight: '4px solid #eee',
-  },
-
-  headerColor: {
-    backgroundColor: '#01A5A3',
+  headerRow: {
+    backgroundColor: colors.accent,
+    borderLeft: `4px solid ${colors.accent}`,
+    borderRight: `4px solid ${colors.accent}`,
     color: '#fff',
   },
-  cell: {
-    borderRight: '1px solid #ccc',
-    padding: '4px',
-    textAlign: 'center',
-    backgroundColor: '#fff',
-    // color: '#000',
+  playerRow: {
+    backgroundColor: colors.gray,
+    borderLeft: `4px solid ${colors.gray}`,
+    borderRight: `4px solid ${colors.gray}`,
   },
-
+  cell: {
+    ...cellBase
+  },
+  cellStop: {
+    ...cellBase,
+    borderRight: '0px'
+  },
+  borderBottom: {
+    borderBottom: `1px solid ${colors.darkGray}`,
+  },
+  scoreCell: {
+    textAlign: 'center',
+  },
+  scoreCellTotal: {
+    textAlign: 'center',
+    width: '52px',
+  },
+  white: {
+    backgroundColor: colors.white,
+  },
+  gray: {
+    backgroundColor: colors.gray,
+  },
+  transparent: {
+    backgroundColor: 'transparent',
+  },
+  alignLeft: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  }
 }
 
 export default style
