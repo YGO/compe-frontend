d6fe7a798083f8893f9350b7ed6508c480ad9354
Akira Sosa <akirasosa@gmail.com>
2017-10-02 15:33:48 +0700

    It rendered EntryListItem many times when change score.
    Still, has a problem in loading.

--- src/routes/admin/competition-show/entry-list-item.component.js
+++ src/routes/admin/competition-show/entry-list-item.component.js
@@ -2,104 +2,117 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import style from './entry-list.styles'
 import classNames from 'classnames'
+import isEqual from 'lodash.isequal'
 
-const EntryListItem = ({
-                         id,
-                         name,
-                         retired,
-                         isEditing,
-                         roundEntries,
-                         pars,
-                         loading,
-                         onClickEdit,
-                         onClickCancel,
-                         onClickSave,
-                         onChangeScore,
-                         onChangeRetired,
-                       }) => {
-  const totalStrokes = roundEntries.map(re =>
-    re.strokes.map(Number).reduce((a, b) => a + b, 0)
-  ).reduce((a, b) => a + b, 0)
+class EntryListItem extends React.Component {
+  shouldComponentUpdate (nextProps, nextState) {
+    if (this.props.retired !== nextProps.retired) return true
+    if (this.props.isEditing !== nextProps.isEditing) return true
+    if (this.props.loading !== nextProps.loading) return true
+    if (!isEqual(this.props.roundEntries, nextProps.roundEntries)) return true
+    return false
+  }
 
-  const totalScore = roundEntries.map(re =>
-    re.strokes.map(Number).reduce((sum, v, idx) => {
-      if (v === 0) return sum
-      return sum + (v - pars[idx])
-    }, 0)
-  ).reduce((a, b) => a + b, 0)
+  render () {
+    const {
+      id,
+      name,
+      retired,
+      isEditing,
+      roundEntries,
+      pars,
+      loading,
+      onClickEdit,
+      onClickCancel,
+      onClickSave,
+      onChangeScore,
+      onChangeRetired,
+    } = this.props
 
-  return (
-    <div className='row'>
-      <div className='col'>
-        <div className='row' style={style.entryRow}>
-          <div className='col-auto'>
-            <span className='font-weight-bold'>{name}</span>
-          </div>
-          <div className='col-auto'>{totalStrokes} ({totalScore})</div>
-          <div className='col-auto mr-auto'>
-            <div
-              className={classNames('form-check', {disabled: !isEditing || loading})}>
-              <label className='form-check-label'>
-                <input className='form-check-input' type='checkbox'
-                       checked={retired} disabled={!isEditing || loading}
-                       onChange={e => onChangeRetired(e.target.checked)}/> 棄権
-              </label>
+    const totalStrokes = roundEntries.map(re =>
+      re.strokes.map(Number).reduce((a, b) => a + b, 0)
+    ).reduce((a, b) => a + b, 0)
+
+    const totalScore = roundEntries.map(re =>
+      re.strokes.map(Number).reduce((sum, v, idx) => {
+        if (v === 0) return sum
+        return sum + (v - pars[idx])
+      }, 0)
+    ).reduce((a, b) => a + b, 0)
+
+    return (
+      <div className='row'>
+        <div className='col'>
+          <div className='row' style={style.entryRow}>
+            <div className='col-auto'>
+              <span className='font-weight-bold'>{name}</span>
             </div>
+            <div className='col-auto'>{totalStrokes} ({totalScore})</div>
+            <div className='col-auto mr-auto'>
+              <div
+                className={classNames('form-check', {disabled: !isEditing || loading})}>
+                <label className='form-check-label'>
+                  <input className='form-check-input' type='checkbox'
+                         checked={retired} disabled={!isEditing || loading}
+                         onChange={e => onChangeRetired(e.target.checked)}/> 棄権
+                </label>
+              </div>
+            </div>
+            {!isEditing &&
+            <div className='col-auto'>
+              <button type='button' className='btn btn-link' style={style.ctrlBtn}
+                      onClick={() => onClickEdit(id)}
+                      disabled={loading}>スコア編集
+              </button>
+            </div>
+            }
+            {isEditing &&
+            <div className='col-auto'>
+              <button type='button' className='btn btn-link' style={style.ctrlBtn}
+                      onClick={onClickCancel} disabled={loading}>
+                キャンセル
+              </button>
+              <button type='button' className='btn btn-primary btn-sm'
+                      style={style.ctrlBtn} onClick={onClickSave}
+                      disabled={loading}>保存
+              </button>
+            </div>
+            }
           </div>
-          {!isEditing &&
-          <div className='col-auto'>
-            <button type='button' className='btn btn-link' style={style.ctrlBtn}
-                    onClick={() => onClickEdit(id)}
-                    disabled={loading}>スコア編集
-            </button>
-          </div>
-          }
-          {isEditing &&
-          <div className='col-auto'>
-            <button type='button' className='btn btn-link' style={style.ctrlBtn}
-                    onClick={onClickCancel} disabled={loading}>
-              キャンセル
-            </button>
-            <button type='button' className='btn btn-primary btn-sm'
-                    style={style.ctrlBtn} onClick={onClickSave}
-                    disabled={loading}>保存
-            </button>
-          </div>
-          }
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <table className='table table-sm table-bordered'
-                   style={style.scoreTable}>
-              <thead>
-              <tr>
-                {[...Array(18)].map((_, idx) =>
-                  <th key={`EntryListItem-h${idx}`}
-                      style={style.scoreTableHeader}>{idx + 1}</th>
-                )}
-                <th style={style.scoreTableHeader}>合計</th>
-              </tr>
-              </thead>
-              <tbody>
-              {roundEntries.map((re, idx) =>
-                <tr key={`EntryListItem-${re.id}`}>
-                  {re.strokes.map((v, idx) =>
-                    <td key={`EntryListItem-${re.id}-${idx}`}>
-                      <input type='text' value={v} style={style.scoreInput}
-                             disabled={!isEditing || loading}
-                             onChange={e => onChangeScore(re.id, idx, e.target.value)}/>
-                    </td>
+          <div className='row'>
+            <div className='col'>
+              <table className='table table-sm table-bordered'
+                     style={style.scoreTable}>
+                <thead>
+                <tr>
+                  {[...Array(18)].map((_, idx) =>
+                    <th key={`EntryListItem-h${idx}`}
+                        style={style.scoreTableHeader}>{idx + 1}</th>
                   )}
-                  <td>{re.strokes.map(Number).reduce((a, b) => a + b, 0)}</td>
+                  <th style={style.scoreTableHeader}>合計</th>
                 </tr>
-              )}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                {roundEntries.map((re, idx) =>
+                  <tr key={`EntryListItem-${re.id}`}>
+                    {re.strokes.map((v, idx) =>
+                      <td key={`EntryListItem-${re.id}-${idx}`}>
+                        <input type='text' value={v} style={style.scoreInput}
+                               disabled={!isEditing || loading}
+                               onChange={e => onChangeScore(re.id, idx, e.target.value)}/>
+                      </td>
+                    )}
+                    <td>{re.strokes.map(Number).reduce((a, b) => a + b, 0)}</td>
+                  </tr>
+                )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 EntryListItem.defaultProps = {
