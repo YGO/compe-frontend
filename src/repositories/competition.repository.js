1d550e7daa1387902ff3b4a290fe6d12313b60dc
Akira Sosa <akirasosa@gmail.com>
2017-10-02 11:59:47 +0700

    apply production

--- src/repositories/competition.repository.js
+++ src/repositories/competition.repository.js
@@ -1,16 +1,8 @@
-import competitions from '../mock/competitions'
+import { API_BASE_URL } from './index'
 
-// const API_URL = `${API_BASE_URL}/competitions`
+const API_URL = `${API_BASE_URL}/competitions`
 
 export const getCompetition = (id) => {
-  const c = competitions.find(c => c.id === id)
-  return Promise.resolve(c)
-
-  // return fetch(`${API_URL}/${id}`)
-  //   .then(res => res.json(null))
-  //   .then(competition => ({
-  //     ...competition,
-  //     rounds: competition.rounds.sort((a, b) => a.play_order > b.play_order ? 1 : -1),
-  //     scores: competition.scores.sort((a, b) => a.id > b.id ? 1 : -1)
-  //   }))
+  return fetch(`${API_URL}/${id}`)
+    .then(res => res.json(null))
 }
